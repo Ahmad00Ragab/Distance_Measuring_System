cf01d632ff3c2f29eb67b5751d745e62a40396d9
Ahmad Haroun <109513775+Ahmad00Ragab@users.noreply.github.com>
2023-09-28 05:49:33 +0300

    Add files via upload

---
+++ WorkSpace/Debug/main.d
@@ -0,0 +1,14 @@
+main.o main.o: ../main.c ../ultrasonic.h ../std_types.h ../lcd.h \
+  ../timer1.h ../gpio.h ../common_macros.h
+
+../ultrasonic.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../timer1.h:
+
+../gpio.h:
+
+../common_macros.h:
---
+++ WorkSpace/Debug/ultrasonic.d
@@ -0,0 +1,10 @@
+ultrasonic.o ultrasonic.o: ../ultrasonic.c ../timer1.h ../gpio.h \
+  ../std_types.h ../common_macros.h
+
+../timer1.h:
+
+../gpio.h:
+
+../std_types.h:
+
+../common_macros.h:
